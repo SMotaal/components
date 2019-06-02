6ac482b607193a4f9830edad33a2dbb2de3074ce
Saleh <saleh@grasppe.com>
2019-06-02 14:46:26 -0400

    Create builtins-headings.js

---
+++ examples/snippets/builtins-headings.js
@@ -0,0 +1,17 @@
+﻿//* TEST: **
+var hindex;
+customElements.define(
+  `h-${(hindex = 1 + (hindex || 0))}`,
+  class extends HTMLHeadingElement {
+    connectedCallback() {
+      this.innerText = this.localName;
+    }
+  },
+  {extends: 'h1'},
+) || document.body.prepend(new (customElements.get(`h-${hindex}`))());
+
+//* TEST: **
+((e = document.createElement('h1')) => (
+  e.attachShadow({mode: 'open'}).append('<shadow>', document.createElement('slot')),
+  document.body.prepend((e.append(`<${e.localName}>`), e))
+))();
