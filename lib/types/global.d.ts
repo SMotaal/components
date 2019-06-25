247a6631b09abdfd10f45f8b680973110b6c2769
Saleh <saleh@grasppe.com>
2019-06-24 20:20:59 -0400

    Refactoring for Markout Playground concept proof

--- lib/types/global.d.ts
+++ lib/types/global.d.ts
@@ -1,23 +1,35 @@
 ﻿//@ts-check
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="esnext" />
+// / <reference lib="dom" />
+// / <reference lib="dom.iterable" />
+// / <reference lib="es2020" />
 
-interface ImportMeta {
-  url: string;
-}
+import {css} from '../css';
+import {html, template} from '../templates';
+import {Component} from '../component';
+import {Attributes} from '../attributes';
+import {Assets} from '../assets';
 
-interface Globals {}
+declare global {
+  interface ImportMeta {
+    url: string;
+    ['components.css']?: typeof css;
+    ['components.html']?: typeof html;
+    ['components.template']?: typeof template;
+    ['components.Assets']?: typeof Assets;
+    ['components.Attributes']?: typeof Attributes;
+    ['components.Component']?: typeof Component;
+  }
 
-interface DocumentOrShadowRoot {
-  activeElement: HTMLElement;
-}
+  interface Globals {}
 
-interface EventListenerOptions {
-  passive: boolean;
-  once: boolean;
-}
+  interface DocumentOrShadowRoot {
+    activeElement: HTMLElement;
+  }
 
-interface Attributes {
+  interface EventListenerOptions {
+    passive: boolean;
+    once: boolean;
+  }
 
+  interface Attributes {}
 }
