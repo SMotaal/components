4764cf32060203cd4bdb9aa7a3617407a9de9779
Saleh <daflair@daflairs.local>
2019-08-27 11:00:40 +0200

    Upstream Markout helpers (declarativeStyling… etc.)

--- lib/types/global.d.ts
+++ lib/types/global.d.ts
@@ -1,23 +1,23 @@
 ﻿//@ts-check
-// / <reference lib="dom" />
-// / <reference lib="dom.iterable" />
-// / <reference lib="es2020" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="es2020" />
 
-import {css} from '../css';
-import {html, template} from '../templates';
-import {Component} from '../component';
-import {Attributes} from '../attributes';
-import {Assets} from '../assets';
+// import {css} from '../css';
+// import {html, template} from '../templates';
+// import {Component} from '../component';
+// import {Attributes} from '../attributes';
+// import {Assets} from '../assets';
 
 declare global {
   interface ImportMeta {
     url: string;
-    ['components.css']?: typeof css;
-    ['components.html']?: typeof html;
-    ['components.template']?: typeof template;
-    ['components.Assets']?: typeof Assets;
-    ['components.Attributes']?: typeof Attributes;
-    ['components.Component']?: typeof Component;
+    // ['components.css']?: import('./templates.js')['css'];
+    // ['components.html']?: import('./templates.js')['html'];
+    // ['components.template']?: import('./templates.js')['template'];
+    // ['components.Assets']?: import('./assets.js')['Assets'];
+    // ['components.Attributes']?: import('./attributes.js')['Attributes'];
+    // ['components.Component']?: import('./component.js')['Component'];
   }
 
   interface Globals {}
