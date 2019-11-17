fa81b715eaadc41ad184b1f009053de3ca8e7093
Saleh Abdel Motaal <saleh@smotaal.io>
2019-11-17 12:23:32 -0500

    Housekeeping

--- packages/pseudom/pseudom.js
+++
@@ -1,9 +0,0 @@
-import * as pseudo from './lib/pseudo.js';
-import * as dom from './lib/native.js';
-
-// TEST: Trace for ESM testing
-typeof process === 'object' && console.info('[ESM]: %o', import.meta.url);
-
-export const native = dom.document && dom;
-export const {createElement, createText, createFragment} = native || pseudo;
-export {pseudo};
